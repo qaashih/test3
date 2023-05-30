003e05d9dd2a4956784b4e90e671f105c2a85ebd
Test Ashih <qaashihqa@gmail.com>
2023-05-30 21:37:44 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int price){
+        int quantityBonus = price / 20;
+        return quantityBonus;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 5_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+    }
+}
